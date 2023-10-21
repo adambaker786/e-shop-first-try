c8c5be13095c5d8fcc2bb77edfb37819d246c645
adambaker786 <adambeno@mail.ru>
2023-10-21 17:09:52 +0200

    working on logo page

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header(props) {
+  return (
+    <header>
+      <div>
+        <span className="logo">House Staff</span>
+      </div>
+      <div className="presentation"></div>
+    </header>
+  );
+}
+
+export default Header;

5cc3ad330938523033c7766d4e0aea4012611236
adambaker786 <adambeno@mail.ru>
2023-10-22 16:26:28 +0200

    Working with item

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
+import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Items from "./components/Items";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Grey chair',
+          img: 'grey-chair.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+          category: 'chairs',
+          price: '49.99'
+        },
+        {
+          id: 2,
+          title: 'Table',
+          img: 'table.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+          category: 'tables',
+          price: '49.99'
+        },
+        {
+          id: 3,
+          title: 'Sofa',
+          img: 'sofa.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+          category: 'sofa',
+          price: '549.99'
+        },
+        {
+          id: 4,
+          title: 'Lamp',
+          img: 'lamp.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+          category: 'light',
+          price: '29.99'
+        },
+        {
+          id: 5,
+          title: 'Bed',
+          img: 'bed.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+          category: 'beds',
+          price: '249.99'
+        },
+      ]
+    }
+  }
+  render() {
   return (
     <div className="wrapper">
       <Header></Header>
+      <Items items={this.state.items}></Items>
       <Footer></Footer>
     </div>
   );
+ }
 }
 
 export default App;
